775fe07dea92ed4a16004dea559a0df624b8b499
Artem Romanovich <art.rom.one@gmail.com>
2024-03-16 17:07:29 +0300

    conf lunaline

--- lua/plugins/configs/lualine_config.lua
+++ lua/plugins/configs/lualine_config.lua
@@ -3,66 +3,77 @@
 local M = {}
 
 function M.setup()
-    require('lualine').setup {
-  options = {
-    icons_enabled = true,
-    theme = 'auto',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
-    },
-    ignore_focus = {},
-    always_divide_middle = true,
-    globalstatus = false,
-    refresh = {
-      statusline = 1000,
-      tabline = 1000,
-      winbar = 1000,
+    require("lualine").setup {
+        options = {
+            icons_enabled = true,
+            theme = "auto",
+            component_separators = {left = "", right = ""},
+            section_separators = {left = "", right = ""},
+            disabled_filetypes = {"NvimTree", "tagbar"},
+            ignore_focus = {},
+            always_divide_middle = true,
+            globalstatus = false,
+            refresh = {
+                statusline = 1000,
+                tabline = 1000,
+                winbar = 1000
+            }
+        },
+        sections = {
+            lualine_a = {"branch"},
+            lualine_b = {"diff", "filename", "filesize", "encoding"},
+            lualine_c = {
+                {
+                    function()
+                        local msg = "No Active Lsp"
+                        local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+                        local clients = vim.lsp.get_active_clients()
+                        if next(clients) == nil then
+                            return msg
+                        end
+                        for _, client in ipairs(clients) do
+                            local filetypes = client.config.filetypes
+                            if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+                                return client.name
+                            end
+                        end
+                        return msg
+                    end,
+                    -- icon = ' ',
+                    color = {}
+                }
+            },
+            lualine_x = {
+                {
+                    "diagnostics",
+                    sources = {"nvim_diagnostic", "nvim_lsp", "nvim_workspace_diagnostic"},
+                    symbols = {error = " ", warn = " ", info = " "},
+                    diagnostics_color = {
+                        -- Same values as the general color option can be used here.
+                        error = "DiagnosticError", -- Changes diagnostics' error color.
+                        warn = "DiagnosticWarn", -- Changes diagnostics' warn color.
+                        info = "DiagnosticInfo", -- Changes diagnostics' info color.
+                        hint = "DiagnosticHint" -- Changes diagnostics' hint color.
+                    },
+                    colored = true
+                }
+            },
+            lualine_y = {"progress"},
+            lualine_z = {"location"}
+        },
+        inactive_sections = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = {"filename"},
+            lualine_x = {"location"},
+            lualine_y = {},
+            lualine_z = {}
+        },
+        tabline = {},
+        winbar = {},
+        inactive_winbar = {},
+        extensions = {}
     }
-  },
-  sections = {
-    -- lualine_a = {'mode'},
-    lualine_a = {
-    {
-      function()
-    local msg = 'No Active Lsp'
-    local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-    local clients = vim.lsp.get_active_clients()
-    if next(clients) == nil then
-      return msg
-    end
-    for _, client in ipairs(clients) do
-      local filetypes = client.config.filetypes
-      if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-        return client.name
-      end
-    end
-    return msg
-  end,
-  icon = ' LSP:',
-  color = { fg = '#ffffff', gui = 'bold' },
-  },},
-    lualine_b = {'branch', 'diff'},
-    lualine_c = {'filename', 'filesize'},
-    lualine_x = {'encoding'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  winbar = {},
-  inactive_winbar = {},
-  extensions = {}
-}
 end
 
 return M
